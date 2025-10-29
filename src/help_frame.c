15eea70e89cd0835a730e0966740b998e1ef5046
Baptiste-Hdsa <baptiste.hdsa@gmail.com>
2025-10-29 17:14:11 +0100

    Refactor HelpFrame to improve title positioning and add explanation text for letter A

--- src/help_frame.c
+++ src/help_frame.c
@@ -9,6 +9,7 @@
 #include "container.h"
 #include "element.h"
 #include "frame.h"
+#include "image.h"
 #include "input.h"
 #include "list.h"
 #include "logger.h"
@@ -44,25 +45,38 @@ HelpFrame *HelpFrame_new(App *app) {
 }
 
 static void HelpFrame_addElements(HelpFrame *self, App *app) {
-    ListIterator* it = ListIterator_new(self->elements);
+    ListIterator *it = ListIterator_new(self->elements);
     while (ListIterator_hasNext(it)) {
-        Element* element = ListIterator_next(it);
+        Element *element = ListIterator_next(it);
         Element_destroy(element);
     }
     ListIterator_destroy(it);
-    List_clear(self->elements);    TextStyle *text_style = TextStyle_new(
+    List_clear(self->elements);
+    TextStyle *text_title_style = TextStyle_new(
         ResourceManager_getDefaultBoldFont(app->manager, 36),
         36, COLOR_WHITE, TTF_STYLE_NORMAL);
 
     int w, h;
-    SDL_GetWindowSize(self->app->window, &w ,&h);
-    Text *title_text = Text_new(app->renderer, text_style, Position_new(w / 2, 0), true, "Help");
-    Size size_window = Text_getSize(title_text);
-    Text_setPosition(title_text, w/2, size_window.height /2);
+    SDL_GetWindowSize(self->app->window, &w, &h);
+    Text *title_text = Text_new(app->renderer, text_title_style, Position_new(w / 2, 0), true, "Help");
+    Size size_title_text = Text_getSize(title_text);
+    Text_setPosition(title_text, size_title_text.width, size_title_text.height / 2);
     List_push(self->elements, Element_fromText(title_text, NULL));
 
-
-
+    Position *position_A = Position_new(0, (h / 8) * 2);
+    Image *image_A = Image_load(self->app, "letter_A.svg", position_A, false);
+    Image_setRatio(image_A, 0.05);
+    List_push(self->elements, Element_fromImage(image_A, NULL));
+    Size size_image_A = Image_getSize(image_A);
+    TextStyle *text_A_style = TextStyle_new(
+        ResourceManager_getDefaultBoldFont(app->manager, 20),
+        20, COLOR_WHITE, TTF_STYLE_NORMAL);
+    Text *text_A = Text_new(app->renderer,
+                            text_A_style,
+                            Position_new(position_A->x + 10 + size_image_A.width, position_A->y+5),
+                            false,
+                            "explication de A");
+    List_push(self->elements, Element_fromText(text_A, NULL));
 }
 
 void HelpFrame_destroy(HelpFrame *self) {
@@ -81,7 +95,7 @@ void HelpFrame_update(HelpFrame *self) {
     Element_updateList(self->elements);
 
     if (self->app->input->esc) {
-       App_frameBack(self->app);
+        App_frameBack(self->app);
     }
 }
 
